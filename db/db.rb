3fc06315f7c402ddf75689d73bc2a70cf8489301
ttheunta <timothytheunissen@outlook.com>
2022-09-14 11:00:45 +0930

    changed db name

--- db/db.rb
+++ db/db.rb
@@ -1,7 +1,7 @@
 require 'pg'
 
 def run_sql(sql, sql_params = [])
-  db = PG.connect(dbname: 'd_and_e_db')
+  db = PG.connect(ENV['DATABASE_URL'] || {dbname: 'd_and_e_db'})
   results = db.exec_params(sql, sql_params)
   db.close
   results
